7b977c4a475d0d995263fcdd5dfeb56cbb37a3c8
Ranju Kumari <ranjukumari0128@gmail.com>
2026-03-20 09:22:25 +0530

    Refactor maximum subarray algorithms with comments
    Refactored maxSum_KadanesAlgo .

--- DynamicProgramming/LeetCode/MaximumSubarray_LC_53.java
+++ DynamicProgramming/LeetCode/MaximumSubarray_LC_53.java
@@ -11,36 +11,61 @@ public class MaximumSubarray_LC_53 {
 
     }
 
-    public static int maxSum_BigOofNsquareSoln(int [] a){
 
-        int maxSum = Integer.MIN_VALUE;
-        int n = a.length;
+//Approach:
+    //This uses Kadane’s Algorithm.
+    //It keeps a running sum and resets it when it becomes negative.
+//How it works:
+    //Add current element to sum.
+    //Update maxSum if current sum is better.
+    //If sum becomes negative → reset to 0 (because a negative prefix will only hurt future sums).
+    //O(n) (single loop)
+
+//Insight:
+   //It avoids unnecessary work by discarding subarrays that would reduce future sums.
+   //It’s an optimized greedy approach. 
+    public static long maxSum_KadanesAlgo(int [] nums){
         int sum = 0;
-        for(int i=0; i<=n-1;i++){
-            sum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
-            for(int j=i; j<=n-1;j++){
-                sum+=a[j];
-                maxSum = max(sum, maxSum);
-            }
-        }
-        return maxSum;
-    }
-
-    public static long maxSum_KadanesAlgo(int [] a){
-
-        long sum=0;
-        long maxSum = -1;
-        int n=a.length;
-        for(int i =0;i<=n-1;i++){
-            sum+=a[i];
-            if(sum>maxSum){
-                maxSum = sum;
-            }
+        for(int i=0;i<=nums.length-1;i++){
+            sum = sum+nums[i];
+            maxSum = Math.max(sum, maxSum);
             if(sum<0){
                 sum=0;
             }
         }
         return maxSum;
     }
+
+
+
+//Brute force approach : O(n^2)
+//Approach:
+    //This checks all possible subarrays.
+//How it works:    
+    //Outer loop picks starting index i.
+    //Inner loop builds subarrays from i to end.
+    //Computes sum of every possible subarray.
+    //O(n²) (nested loops)
+
+    public static int maxSum_BigOofNsquareSoln(int [] nums){
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i=0;i<=nums.length-1;i++){
+            sum=0;
+            for(int j=i;j<=nums.length-1;j++){
+                sum = sum+nums[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+
+
+
+
+    
+
 }
